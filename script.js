b2b2a2860c61894dacc54d2e9e8816b8c49ae592
LorieNatalie <lorienatalienoelle@gmail.com>
2025-05-14 11:26:25 +0200

    improved the website, now has a picture
    website has more information

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Welcome to My Blog!");
+// When the page loads, this code runs
+const button = document.getElementById('codeButton');
+
+button.addEventListener('click', () => {
+  alert('Embrace the allure of code like the charming temptress you are — captivating challenges with confidence 💻✨, weaving magic into every line 🧙‍♀️📝, and turning every bug into a beautiful masterpiece 🐞🎨. Keep coding with passion ❤️🔥, and let your creativity be your irresistible charm! 😊💖 Keep shining and happy coding! 🚀😊');
+});
+
